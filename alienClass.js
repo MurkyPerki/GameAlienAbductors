d38f49423d5138c29659323782fd4f760d4c9117
MurkyPerki <156456239+MurkyPerki@users.noreply.github.com>
2024-10-24 04:50:58 +0200

    health_andShootingAliens

--- alienClass.js
+++ alienClass.js
@@ -25,7 +25,7 @@ class Aliens {
 
 
 
-    show() {
+    render() {
 
         noStroke();
         // Change color based on health
@@ -78,38 +78,87 @@ class Aliens {
 
 
 
-class shootingAlien extends Aliens{
+class ShootingAlien extends Aliens{
 
     constructor(x,y,sizeX, sizeY){
+        super(x, y, sizeX, sizeY)
 
         this.sizeX = sizeX
         this.sizeY = sizeY
         this.x = x
         this.y = y
 
-        this.shootInterval
+        this.shootInterval = 120
+        this.lastShot = frameCount;
     }
 
 
 
 
-    // update(){
-    //     this.x += this.moveSpeed
+    update(){
+        super.update();
+        this.shoot();
 
-    // }
+    }
 
 
+
+    shoot(){
+
+        if (frameCount - this.lastShot >= this.shootInterval) {
+            const bullet = new AlienBullet(this.x, this.y + this.sizeY / 2);
+            alienBullets.push(bullet);
+            const bulletBox = new AABB(bullet.x, bullet.y, bullet.sizeX, bullet.sizeY);
+            alienBulletBoxes.push(bulletBox);
+      
+            this.lastShot = frameCount;
+
+
+        }
+    }
 
     render(){
 
-
-        fill(200,10,10)
-        rectMode(CENTNER)
-        rect(this.x,this.y,this.size)
+        noStroke()
+        fill(10,10,200)
+        rectMode(CENTER)
+        rect(this.x,this.y,this.sizeX,this.sizeY)
 
 
 
     }
+
+
+}
+
+
+
+
+class AlienBullet{
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sizeX = 5;
+        this.sizeY = 10;
+        this.speed = 6; // Positive to move downwards
+      }
+    
+      update() {
+        this.y += this.speed;
+      }
+    
+      offScreen() {
+        return this.y > canvasHeight;
+      }
+    
+      render() {
+        fill(255, 0, 0); // Red bullet
+        noStroke();
+        rectMode(CENTER);
+        rect(this.x, this.y, this.sizeX, this.sizeY);
+      }
+
 
 
 }
